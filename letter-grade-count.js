fd5dc734948681d265b92fa50dab00363ef0688d
Kazi Reazul Islam <kazireazul1999@gmail.com>
2021-01-17 21:57:30 +0600

    first commit

---
+++ letter-grade-count.js
@@ -0,0 +1,52 @@
+var marks = prompt("Enter Your Letter Marks :");
+    
+/* 
+if (marks >= 101)
+console.log("Invalid Marks");
+
+else if (marks >= 80)
+    console.log("A+");
+    
+else if (marks >= 70)
+    console.log("A");
+
+else if (marks >= 60)
+    console.log("A-");
+
+else if (marks >= 50)
+    console.log("B");
+else if (marks >= 40)
+    console.log("D");
+else if (marks >= 33)
+    console.log("You Pass In The Exam");
+else
+    console.log("Fail");
+*/
+
+// Using Logical Oparator
+// Students Marks Count
+if (marks >= 101)
+console.log("Invalid Marks");
+
+else if (marks >= 80 && marks <=100)
+    console.log("A+");
+    
+else if (marks >= 70 && marks <=79 )
+    console.log("A");
+
+else if (marks >= 60 && marks <=69)
+    console.log("A-");
+
+else if (marks >= 50 && marks <=69)
+    console.log("B");
+else if (marks >= 40 && marks <=49)
+    console.log("D");
+else if (marks >= 33 && marks <=40)
+    console.log("You Pass In The Exam");
+else
+    console.log("Fail");
+
+
+
+
+
